1a14ed2a910de677515e959577999397608e8c8f
Nancy <nancypanghal13@gmail.com>
2025-11-19 12:28:02 +0530

    Updates

--- index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './landing.css';   // optional - if you want styles
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <h1 style={{textAlign:'center', marginTop:'200px', fontSize:'50px', color:'green'}}>
-    YES! YOUR REAL PAGE WORKS NOW 🚀
-  </h1>
-);
