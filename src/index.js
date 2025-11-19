ebe2010910c7b4cadc5bf71d0433f5f319602699
Nancy <nancypanghal13@gmail.com>
2025-11-19 12:27:31 +0530

    src dir deleted

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './landing.css';   // optional - if you want styles
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <h1 style={{textAlign:'center', marginTop:'200px', fontSize:'50px', color:'green'}}>
